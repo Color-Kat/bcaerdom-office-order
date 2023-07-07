4eeb6764e5ee6be23a5d3b492fbe1ecdde258c47
Color-Kat <katik7979@gmail.com>
2023-07-07 17:51:58 +0300

    bolshevik-center captcha+policy

---
+++ app/Mail/NewCallRequestMail.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class NewCallRequestMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    private $data;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        $this->data = array_merge(
+            $data,
+            []
+        );
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Новая заявка ' . env('VITE_BC_NAME'))
+            ->view('emails.newCallRequest', [
+                'data' => $this->data
+            ]);
+    }
+}
